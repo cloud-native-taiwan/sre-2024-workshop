714284ad3deb0172f6d7700d337432390c47991a
HungWei Chiu <sppsorrg@gmail.com>
2024-04-11 23:33:58 +0800

    Update yaml for q6,q7

--- q6/app/init.sh
+++ q6/app/init.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -xe
+set -e
 
 cleanup() {
     echo "Cleaning up..."
@@ -7,11 +7,10 @@ cleanup() {
 }
 
 trap cleanup INT TERM
-DATA_PATH=${DATA_PATH:-/tmp/}
-
-dd if=/dev/zero of=$DATA_PATH/file bs=1M count=2048
+DATA_PATH=${DATA_PATH:-/tmp/file}
 
 while :; do
     echo "Hello! ${SECONDS} secs elapsed..."
-    sleep 1s
+    cat $DATA_PATH
+    sleep 5s
 done
